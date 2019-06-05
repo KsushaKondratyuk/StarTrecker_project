b9ee8bb6174828d2cbee965d524e9793488c1242
Ksusha <ksusha123456789@gmail.com>
2019-06-05 23:55:46 +0300

    add hazelcast-cache-client

---
+++ hazelcast-cache-client/src/main/java/com/duplicate/microservices/hazelcast/cache/HazelcastClientConfig.java
@@ -0,0 +1,29 @@
+package com.duplicate.microservices.hazelcast.cache;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HazelcastClientConfig {
+
+    @Bean
+    public HazelcastInstance hazelcastInstance() {
+        ClientConfig cfg = new ClientConfig();
+
+        ClientNetworkConfig network = cfg.getNetworkConfig();
+        network.addAddress("hazelcast-cache", "hazelcast-cache:5701");
+
+
+        return HazelcastClient.newHazelcastClient(cfg);
+    }
+
+    @Bean
+    public HazelcastClientTemplate hazelcastClientTemplate() {
+        return new HazelcastClientTemplate(hazelcastInstance());
+    }
+
+}
